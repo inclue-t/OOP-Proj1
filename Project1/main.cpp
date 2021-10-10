732ed833045b356f58635e9db7e01c10fd45cd8f
inclue <wb092311@naver.com>
2021-10-10 22:47:39 +0900

    전체적인 수정 진행

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,7 +6,8 @@
 #include <string>
 #include <vector>
 
-class Student {
+class Student
+{
 public:
 	std::string name;
 	int student_id;
@@ -14,7 +15,8 @@ public:
 	std::string department;
 	std::string telephone;
 
-	Student(std::string line) { 
+	Student(std::string line)
+	{ 
 		std::vector<std::string> parse;
 		std::istringstream ss(line);
 		std::string token;
@@ -26,6 +28,7 @@ public:
 		department = parse[3];
 		telephone = parse[4];
 	}
+
 	Student(std::string nm, int stdid, int birth, std::string depart, std::string tel)
 	{
 		name = nm;
@@ -36,7 +39,8 @@ public:
 
 	}
 
-	std::string GetStudent() {
+	std::string GetStudent()
+	{
 		return name + "," +
 			std::to_string(student_id) + "," +
 			std::to_string(birth_year) + "," +
@@ -48,16 +52,19 @@ public:
 std::string file;
 std::vector<Student> student_info;
 
-void ReadAll() {
+void ReadAll()
+{
 	std::ifstream fin(file);
-	if (!fin.is_open()) {
+	if (!fin.is_open())
+	{
 		std::cout << "Since the file doesn't exist, it creates a new file.\n";
 		std::ofstream make_file(file);
 		make_file.close();
 		return;
 	}
 
-	while (!fin.eof()) {
+	while (!fin.eof())
+	{
 		std::string line;
 		std::getline(fin, line);
 		if (line.empty()) break;
@@ -67,13 +74,15 @@ void ReadAll() {
 	fin.close();
 }
 
-void WriteAll() {
+void WriteAll()
+{
 	std::ofstream fout(file);
 	for (Student student : student_info) fout << student.GetStudent() << std::endl;
 	fout.close();
 }
 
-void WriteLine(Student student) {
+void WriteLine(Student student)
+{
 	student_info.push_back(student);
 	std::ofstream fout(file, std::ios::out | std::ios::app);
 	fout << student.GetStudent() << std::endl;
@@ -205,48 +214,29 @@ void Insertion()
 
 }
 
-void Search() 
-{
-	while (true) 
-	{
-		int num = 0;
-		std::cout << "1. Search by name\n2. Search by student ID\n3. Search by admission year\n4. Search by department name\n5. List All\n6. Exit\n";
-		std::cin >> num;
-		
-		if (num == 1) SearchName();
-		else if (num == 2) SearchId();
-		else if (num == 3) SearchYear();
-		else if (num == 4) SearchDepartment();
-		else if (num == 5) SearchAll();
-		else if (num == 6) break;
-		else std::cout << "Please enter the correct number.\n";
-
-	}	
-}
-
 void SearchName() 
 {
 	int num = 0;
 	std::string name;
 
 	std::cout << "Please input your name.\n";
-	std::cin >> name;
+	std::cin.ignore();
+	std::getline(std::cin, name); // 공백도 받게
 
 	for (Student student : student_info) 
 	{
 		if (student.name == name) 
 		{
 			num++;
-			std::cout << student.name + ',' << std::endl;
-			std::cout << student.student_id + ',' << std::endl; 
-			std::cout << student.birth_year + ',' << std::endl; 
-			std::cout << student.department + ',' << std::endl;
+			std::cout << student.name + "," << std::endl;
+			std::cout << std::to_string(student.student_id) + "," << std::endl; 
+			std::cout << std::to_string(student.birth_year) + "," << std::endl; 
+			std::cout << student.department + "," << std::endl;
 			std::cout << student.telephone + '\n' << std::endl;
-
 		}
 	}
 
-	std::cout << "Total" << num << "student was found.\n";
+	std::cout << "Total " << num << " student was found.\n";
 }
 
 void SearchId() 
@@ -268,16 +258,15 @@ void SearchId()
 		if (student.student_id == id) 
 		{
 			num++;
-			std::cout << student.name + ',' << std::endl;
-			std::cout << student.student_id + ',' << std::endl;
-			std::cout << student.birth_year + ',' << std::endl;
-			std::cout << student.department + ',' << std::endl;
+			std::cout << student.name + "," << std::endl;
+			std::cout << std::to_string(student.student_id) + "," << std::endl;
+			std::cout << std::to_string(student.birth_year) + "," << std::endl;
+			std::cout << student.department + "," << std::endl;
 			std::cout << student.telephone + '\n' << std::endl;
-
 		}
 	}
 
-	std::cout << "Total" << num << "student was found.\n";
+	std::cout << "Total " << num << " student was found.\n";
 }
 
 void SearchYear() 
@@ -299,16 +288,15 @@ void SearchYear()
 		if (student.student_id / 1000000 == year) 
 		{
 			num++;
-			std::cout << student.name + ',' << std::endl;
-			std::cout << student.student_id + ',' << std::endl;
-			std::cout << student.birth_year + ',' << std::endl;
-			std::cout << student.department + ',' << std::endl;
+			std::cout << student.name + "," << std::endl;
+			std::cout << std::to_string(student.student_id) + "," << std::endl;
+			std::cout << std::to_string(student.birth_year) + "," << std::endl;
+			std::cout << student.department + "," << std::endl;
 			std::cout << student.telephone + '\n' << std::endl;
-
 		}
 	}
 
-	std::cout << "Total" << num << "student was found.\n";
+	std::cout << "Total " << num << " student was found.\n";
 }
 
 void SearchDepartment()
@@ -324,16 +312,15 @@ void SearchDepartment()
 		if (student.department == department) 
 		{
 			num++;
-			std::cout << student.name + ',' << std::endl;
-			std::cout << student.student_id + ',' << std::endl;
-			std::cout << student.birth_year + ',' << std::endl;
-			std::cout << student.department + ',' << std::endl;
+			std::cout << student.name + "," << std::endl;
+			std::cout << std::to_string(student.student_id) + "," << std::endl;
+			std::cout << std::to_string(student.birth_year) + "," << std::endl;
+			std::cout << student.department + "," << std::endl;
 			std::cout << student.telephone + '\n' << std::endl;
-
 		}		
 	}
 
-	std::cout << "Total" << num << "student was found.\n";
+	std::cout << "Total " << num << " student was found.\n";
 
 }
 
@@ -344,31 +331,49 @@ void SearchAll()
 	for (Student student : student_info) 
 	{		
 		num++;
-		std::cout << student.name + ',' << std::endl;
-		std::cout << student.student_id + ',' << std::endl;
-		std::cout << student.birth_year + ',' << std::endl;
-		std::cout << student.department + ',' << std::endl;
+		std::cout << student.name + "," << std::endl;
+		std::cout << std::to_string(student.student_id) + "," << std::endl;
+		std::cout << std::to_string(student.birth_year) + "," << std::endl;
+		std::cout << student.department + "," << std::endl;
 		std::cout << student.telephone + '\n' << std::endl;
-		
 	}
 
-	std::cout << "Total" << num << "student was found.\n";
+	std::cout << "Total " << num << " student was found.\n";
 
 }
 
-bool Comparename(Student a, Student b)
+void Search()
+{
+	while (true)
+	{
+		int num = 0;
+		std::cout << "1. Search by name\n2. Search by student ID\n3. Search by admission year\n4. Search by department name\n5. List All\n6. Exit\n";
+		std::cin >> num;
+
+		if (num == 1) SearchName();
+		else if (num == 2) SearchId();
+		else if (num == 3) SearchYear();
+		else if (num == 4) SearchDepartment();
+		else if (num == 5) SearchAll();
+		else std::cout << "Please enter the correct number.\n";
+
+		if (num >= 1 && num <= 6) break;
+	}
+}
+
+bool CompareName(Student a, Student b)
 {
 	return a.name < b.name;
 }
-bool Compareid(Student a, Student b)
+bool CompareId(Student a, Student b)
 {
 	return a.student_id < b.student_id;
 }
-bool Compareyear(Student a, Student b)
+bool CompareYear(Student a, Student b)
 {
 	return a.birth_year < b.birth_year;
 }
-bool Comparedepart(Student a, Student b)
+bool CompareDepart(Student a, Student b)
 {
 	return a.department < b.department;
 }
@@ -381,10 +386,10 @@ void Sorting()
 	{
 		std::cout << "- Sorting Option\n1. Sort by Name\n2. Sort by Student ID\n3. Sort by Admission year\n4. Sort by Department namet\n";
 		std::cin >> sortnum;
-		if (sortnum == 1) std::sort(student_info.begin(), student_info.end(), Comparename);
-		else if (sortnum == 2) std::sort(student_info.begin(), student_info.end(), Compareid);
-		else if (sortnum == 3) std::sort(student_info.begin(), student_info.end(), Compareyear);
-		else if (sortnum == 4) std::sort(student_info.begin(), student_info.end(), Comparedepart);
+		if (sortnum == 1) std::sort(student_info.begin(), student_info.end(), CompareName);
+		else if (sortnum == 2) std::sort(student_info.begin(), student_info.end(), CompareId);
+		else if (sortnum == 3) std::sort(student_info.begin(), student_info.end(), CompareYear);
+		else if (sortnum == 4) std::sort(student_info.begin(), student_info.end(), CompareDepart);
 		else std::cout << "invalid order";
 	}
 	std::cout << "Go back to the main menu, after selecting the sorting option..\n";
@@ -393,21 +398,13 @@ void Sorting()
 
 int main(int argc, char** argv) // run.exe stu.txt jjhg ==> argc=3,  run.exe => argc=1
 {
-	if (argc < 2)
-	{
-		//	std::cout << "Please enter a file name.\n";
-		//	return 0;
-		file = "file1.txt";
-	}
-	else
-	{
-		file = argv[1];
-	}
-
+	if (argc < 2) file = "file1.txt";
+	else file = argv[1];
 
 	ReadAll();
 
-	while (true) {
+	while (true)
+	{
 		int num = 0;
 		std::cout << "1. Insertion\n2. Search\n3. Sorting Option\n4. Exit\n";
 		std::cin >> num;
@@ -416,11 +413,9 @@ int main(int argc, char** argv) // run.exe stu.txt jjhg ==> argc=3,  run.exe => 
 		else if (num == 2) Search();
 		else if (num == 3) Sorting();
 		else if (num == 4) break;
-		else std::cout << num << " Please enter the correct number.\n";
+		else std::cout << "Please enter the correct number.\n";
 
 	}
 	std::cout << "Program terminated\n";
 	return 0;
 }
-
-
